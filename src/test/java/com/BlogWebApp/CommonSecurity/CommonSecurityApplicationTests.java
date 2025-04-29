215c9f07a9abba9ca9c5dd441b90360529f767ae
Eylem Abuzeyitoglu <166799047+eylemabuzeyitoglu@users.noreply.github.com>
2025-04-29 18:45:13 +0300

    initial commit

---
+++ src/test/java/com/BlogWebApp/CommonSecurity/CommonSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlogWebApp.CommonSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
