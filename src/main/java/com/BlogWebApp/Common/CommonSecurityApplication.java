4abc453a222c6ffb57980ce602684392079e344e
Eylem Abuzeyitoğlu <166799047+eylemabuzeyitoglu@users.noreply.github.com>
2025-06-02 22:44:40 +0300

    updated

--- src/main/java/com/BlogWebApp/Common/CommonSecurityApplication.java
+++ src/main/java/com/BlogWebApp/Common/CommonSecurityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.BlogWebApp.CommonSecurity.client")
-@ComponentScan(basePackages = "com.BlogWebApp.CommonSecurity")
+@EnableFeignClients(basePackages = "com.BlogWebApp.Common.client")
+@ComponentScan(basePackages = "com.BlogWebApp.Common")
 public class CommonSecurityApplication {
 
 	public static void main(String[] args) {
