67d41b233ab24722108ff3630c23ae7acfc9ec7d
Eylem Abuzeyitoğlu <166799047+eylemabuzeyitoglu@users.noreply.github.com>
2025-06-14 21:29:46 +0300

    Fixed bugs after testing

--- src/main/java/com/BlogWebApp/Common/CommonSecurityApplication.java
+++ src/main/java/com/BlogWebApp/Common/CommonSecurityApplication.java
@@ -1,12 +1,8 @@
 package com.BlogWebApp.Common;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableFeignClients(basePackages = "com.BlogWebApp.Common.client")
 @ComponentScan(basePackages = "com.BlogWebApp.Common")
 public class CommonSecurityApplication {
 
@@ -15,3 +11,4 @@ public class CommonSecurityApplication {
 	}
 
 }
+
